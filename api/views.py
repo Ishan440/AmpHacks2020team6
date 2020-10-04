b7de3096037dadeb8c49745dc433f50347b16916
Ishan Sharma <ishmayoite440@gmail.com>
2020-10-04 08:35:31 -0400

    models defined working on urls and views next

--- api/views.py
+++ api/views.py
@@ -1,69 +1,83 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from listings.models import Listing
-from users.models import Profile
-from listings.forms import LostRegisterForm
-from users.forms import UserCreationForm, UserRegisterForm, UserUpdateForm
-from .serializers import ListingSerializer, ProfileSerializer, UserSerializer
-from django.contrib.auth.models import User
+from django.http import HttpResponse
+from . models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView)
 
 
-@api_view(['GET'])
-def apiOverview(request):
-	api_urls = {
-		'List':'/listing-list/',
-		'User List': '/user-list/',
-		'Detail View':'/listing-detail/<str:pk>/',
-		'Create':'/listing-create/',
-		'Update':'/listing-update/<str:pk>/',
-		'Delete':'/listing-delete/<str:pk>/',
-		}
+# Create your views here.
 
-	return Response(api_urls)
-
-@api_view(['GET'])
-def ListingsList(request):
-	listings = Listing.objects.all().order_by('-id')
-	serializer = ListingSerializer(listings, many=True)
-	return Response(serializer.data)
-
-@api_view(['GET'])
-def UsersList(request):
-	users = User.objects.all()
-	serializer = UserSerializer(User, many=True)
-	return Response(serializer.data)
-@api_view(['GET'])
-def ListingDetail(request, pk):
-	listings = Listing.objects.get(id=pk)
-	serializer = ListingSerializer(listings, many=False)
-	return Response(serializer.data)
+""" Function based views for home and about"""
+def home(request):
+    """
+    :param request: this page was requested
+    :return: http response upon taking the requested route i.e. the view of the page
+    """
+    context = {'posts': Post.objects.all()}
+    # return HttpResponse('<h1> Blog Home </h1>'). We'll return a template instead using render:
+    return render(request, 'blog/home.html', context)
+    # django looks into the templates folder by default.
+    # context will be passed to our template; key name post will be used to access our posts data.
 
 
-@api_view(['POST'])
-def ListingCreate(request):
-	serializer = ListingSerializer(data=request.data)
-
-	if serializer.is_valid():
-		serializer.save()
-
-	return Response(serializer.data)
-
-@api_view(['POST'])
-def ListingUpdate(request, pk):
-	listing = Listing.objects.get(id=pk)
-	serializer = ListingSerializer(instance=listing, data=request.data)
-
-	if serializer.is_valid():
-		serializer.save()
-
-	return Response(serializer.data)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  # by default, django will look in app/model_typeofview.html .
+    context_object_name = 'posts'  # specify what you want to be iterated for the list based view.
+    ordering = ['-date']  # order post view from latest to oldest (hence the minus) as we scroll down.
 
 
-@api_view(['DELETE'])
-def ListingDelete(request, pk):
-	listing = Listing.objects.get(id=pk)
-	listing.delete()
+class PostDetailView(DetailView):
+    model = Post
 
-	return Response('Item succsesfully delete!')
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    "Classes can't use decorators so we inherit the mixin to create post only when someone is looged in."
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):  # override the form validation
+        form.instance.author = self.request.user  # create a post with author as the current logged in user.
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    "The UserPasses mixin checks if the person trying to update the post is the actual author of the post"
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):  # override the form validation
+        form.instance.author = self.request.user  # update a post with author as the current logged in user.
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()  # get the post we're current;y trying to update.
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = 'blog/'  # if post is deleted successfully redirect to blog home.
+
+    def test_func(self):
+        post = self.get_object()  # get the post we're current;y trying to update.
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def about(request):
+    """
+    :param request: this page was requested
+    :return: http response upon taking the requested route i.e. the view of the page
+    """
+    # return HttpResponse('<h1> Blog about </h1>')
+    return render(request, 'blog/about.html', {'title': 'title for about page'})
+
--- employees/apps.py
+++ employees/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class EmployeesConfig(AppConfig):
     name = 'employees'
+    def ready(self):
+        import users.signals
